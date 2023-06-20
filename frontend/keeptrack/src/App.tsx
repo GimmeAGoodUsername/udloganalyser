a27c17cba57ea25001f49765e3070ed5b26248ff
Marcel Michelfelder <marcel.michelfelder@team-parallax.com>
2023-06-20 16:47:01 +0200

    fix: import typo

--- frontend/keeptrack/src/App.tsx
+++ frontend/keeptrack/src/App.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
-import 'semantic-ui-react/semantic.min.css'
+import 'semantic-ui-css/semantic.min.css'
 import './index.css';
 import { AuthProvider } from './context/AuthContext';
 import Login from './home/Login';
 import Navbar from './misc/Navbar';
 function App() {
 
-  return (   
+  return (
     <AuthProvider>
     <Router>
         <Navbar />
@@ -19,7 +19,7 @@ function App() {
           </Routes>
        </Router>
       </AuthProvider>
-     
+
   );
 }
 
