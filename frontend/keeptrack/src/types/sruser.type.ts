c1ca40d47e4843739adf62f258ce16051481b5a5
T G <timgustaw@gmail.com>
2023-07-23 12:21:56 +0200

    added roles

--- frontend/keeptrack/src/types/sruser.type.ts
+++ frontend/keeptrack/src/types/sruser.type.ts
@@ -1,40 +1,39 @@
 import SrOrder from "./order.type";
 import IPlanet from "./planet.type";
 
-export default interface ISrUser{
-    authorities: {
-        authority: Authority
-    },
-    id?:any | null,
+export default interface ISrUser {
+    id?: any | null,
     name?: string,
     race: Race,
     role: Role,
     planets?: IPlanet[],
     order?: SrOrder[],
     delivery?: SrOrder[],
-    isUser: boolean
+    isUser: boolean,
+    authorities: Authority
+
 }
 
-export enum Race{
-    mensch ="mensch",
-    mosoraner="mosoraner",
-    morricaner="morricaner",
-    ozoid="ozoid",
-    zuup="zuup",
-    wegoner="wegoner",
-    plentrop="plentrop",
-    jamazoid="jamazoid",
-    magumer="magumer"
+export enum Race {
+    mensch = "mensch",
+    mosoraner = "mosoraner",
+    morricaner = "morricaner",
+    ozoid = "ozoid",
+    zuup = "zuup",
+    wegoner = "wegoner",
+    plentrop = "plentrop",
+    jamazoid = "jamazoid",
+    magumer = "magumer"
 }
 
-export enum Role{
-    trader="trader",
-    warlord="warlord",
-    freelancer="freelancer"
+export enum Role {
+    trader = "trader",
+    warlord = "warlord",
+    freelancer = "freelancer"
 }
 
-export enum Authority{
-    NONE="NONE",
-    USER="USER",
-    ADMIN="ADMIN"
+export enum Authority {
+    NONE = "NONE",
+    USER = "USER",
+    ADMIN = "ADMIN"
 }
